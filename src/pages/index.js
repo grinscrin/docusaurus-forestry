128545b7fd33e6048610cfff32ecced0c80602cd
grinscrin <andisads@gmail.com>
2022-06-09 18:30:51 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,8 +55,32 @@ function Feature({imageUrl, title, description}) {
 }
 
 function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
   return (
-    
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <header className={classnames('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
   );
 }
 
